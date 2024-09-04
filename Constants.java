5214e97b2c44b5e8252fc2f64260673a8f94fdd9
Ojas Joshi <146631578+Joshiojas185@users.noreply.github.com>
2024-09-04 13:43:51 +0530

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,27 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Constants {
+    static String Welcome = "Welcome to the Program";
+    static String currentString = "Enter the Current String";
+    static String OperationChoices = "Select 1 to append\nSelect 2 to Count the No of Words" +
+            "\nSelect 3 to replace a string\nSelect 4 to find is it a Palindrome\nSelect 5 to splice the " +
+            "String\nSelect 6 to split the String\nSelect 7 to find Maximum Repeating Character\n" +
+            "Select 8 to sort the String\nSelect 9 to shift the Array\nSelect 10 to reverse the String ";
+    static String exit = "Program Exits ";
+    static String InvalidIp = "Choose a valid Input";
+    static String shiftOp = "You've chosen Shift Operation";
+    static String shiftIdx = "Enter the Index of Shifting";
+    static String spliceOp = "You've Chosen Splice Operation";
+    static String appendOp  = "You've Chosen Append Operation";
+    static String replaceOp = "You've Chosen Replace Opearation";
+    static String appendedStr = "Enter The String You want to Append";
+    static String NoOfWords = "The Number of Words in Current String is ";
+    static String startIndex = "Enter starting Index";
+    static String spliceLength = "Enter the Length you want to Splice ";
+    static String maxRepeatingChar = "The Maximum Repeating Character is ";
+    static String sortedString = "The Sorted String is ";
+    static String reversedString = "The Reversed String is ";
+    static String firstChar = "Enter Character A";
+    static String secondChar = "Enter Character B";
+
+}
